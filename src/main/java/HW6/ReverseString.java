4e54c58d003a4372079dff6519bae95487564c30
yurian-v <yurian.canada@gmail.com>
2022-06-23 15:05:38 -0400

    Add few lessons tasks

---
+++ src/main/java/HW6/ReverseString.java
@@ -0,0 +1,3 @@
+package HW6;
+
+public class ReverseString {}
---
+++ src/main/java/HW6/repeatingElement.java
@@ -0,0 +1,19 @@
+package HW6;
+import java.util.Arrays;
+import java.util.Random;
+
+public class repeatingElement {
+public static void main(String[] args) {
+    findDuplicatesMoreEffective();
+}
+    private static void findDuplicatesMoreEffective(){
+
+    }
+//        int[] elements = new int[101];
+//        int i = 0;
+//        for (j = 0; j < elements.length; j++){
+//            elements[j] = i++;
+//        }
+//        element[100] = (int)(math.random)
+}
+
